9d1b23f9934b0ad9b0c3fc945fbd7b066d1e09ca
JenniferG-Parrales <85895809+JenniferG-Parrales@users.noreply.github.com>
2021-12-09 11:59:37 -0600

    Add files via upload

---
+++ ProductManager/MenuFrm.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace ProductManager
+{
+    partial class MenuFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBuscarProductos = new System.Windows.Forms.Button();
+            this.btnRegistrarProductos = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnBuscarProductos
+            // 
+            this.btnBuscarProductos.Location = new System.Drawing.Point(76, 89);
+            this.btnBuscarProductos.Name = "btnBuscarProductos";
+            this.btnBuscarProductos.Size = new System.Drawing.Size(75, 36);
+            this.btnBuscarProductos.TabIndex = 0;
+            this.btnBuscarProductos.Text = "Buscar Productos";
+            this.btnBuscarProductos.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrarProductos
+            // 
+            this.btnRegistrarProductos.Location = new System.Drawing.Point(214, 89);
+            this.btnRegistrarProductos.Name = "btnRegistrarProductos";
+            this.btnRegistrarProductos.Size = new System.Drawing.Size(75, 38);
+            this.btnRegistrarProductos.TabIndex = 1;
+            this.btnRegistrarProductos.Text = "Registrar Productos";
+            this.btnRegistrarProductos.UseVisualStyleBackColor = true;
+            // 
+            // MenuFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 227);
+            this.Controls.Add(this.btnRegistrarProductos);
+            this.Controls.Add(this.btnBuscarProductos);
+            this.Name = "MenuFrm";
+            this.Text = "MenuFrm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscarProductos;
+        private System.Windows.Forms.Button btnRegistrarProductos;
+    }
+}
---
+++ ProductManager/RegistrarFrm.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace ProductManager
+{
+    partial class RegistrarFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtBoxNombre = new System.Windows.Forms.TextBox();
+            this.txtBoxDescripcion = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cmbBoxCategorias = new System.Windows.Forms.ComboBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblVencimiento = new System.Windows.Forms.Label();
+            this.lblCategorias = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.Location = new System.Drawing.Point(56, 298);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 0;
+            this.btnCrear.Text = "Crear";
+            this.btnCrear.UseVisualStyleBackColor = true;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(53, 54);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(53, 92);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 3;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // txtBoxNombre
+            // 
+            this.txtBoxNombre.Location = new System.Drawing.Point(126, 54);
+            this.txtBoxNombre.Name = "txtBoxNombre";
+            this.txtBoxNombre.Size = new System.Drawing.Size(158, 20);
+            this.txtBoxNombre.TabIndex = 5;
+            // 
+            // txtBoxDescripcion
+            // 
+            this.txtBoxDescripcion.Location = new System.Drawing.Point(126, 92);
+            this.txtBoxDescripcion.Name = "txtBoxDescripcion";
+            this.txtBoxDescripcion.Size = new System.Drawing.Size(158, 20);
+            this.txtBoxDescripcion.TabIndex = 6;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(126, 137);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(158, 20);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(126, 173);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // cmbBoxCategorias
+            // 
+            this.cmbBoxCategorias.FormattingEnabled = true;
+            this.cmbBoxCategorias.Items.AddRange(new object[] {
+            "Perecederos",
+            "Lacteos",
+            "Jugos",
+            "Granos"});
+            this.cmbBoxCategorias.Location = new System.Drawing.Point(126, 214);
+            this.cmbBoxCategorias.Name = "cmbBoxCategorias";
+            this.cmbBoxCategorias.Size = new System.Drawing.Size(158, 21);
+            this.cmbBoxCategorias.TabIndex = 9;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(53, 137);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 10;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblVencimiento
+            // 
+            this.lblVencimiento.AutoSize = true;
+            this.lblVencimiento.Location = new System.Drawing.Point(53, 173);
+            this.lblVencimiento.Name = "lblVencimiento";
+            this.lblVencimiento.Size = new System.Drawing.Size(65, 13);
+            this.lblVencimiento.TabIndex = 11;
+            this.lblVencimiento.Text = "Vencimiento";
+            // 
+            // lblCategorias
+            // 
+            this.lblCategorias.AutoSize = true;
+            this.lblCategorias.Location = new System.Drawing.Point(53, 214);
+            this.lblCategorias.Name = "lblCategorias";
+            this.lblCategorias.Size = new System.Drawing.Size(57, 13);
+            this.lblCategorias.TabIndex = 12;
+            this.lblCategorias.Text = "Categorias";
+            // 
+            // RegistrarFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCategorias);
+            this.Controls.Add(this.lblVencimiento);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.cmbBoxCategorias);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtBoxDescripcion);
+            this.Controls.Add(this.txtBoxNombre);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnCrear);
+            this.Name = "RegistrarFrm";
+            this.Text = "RegistrarFrm";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrear;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtBoxNombre;
+        private System.Windows.Forms.TextBox txtBoxDescripcion;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cmbBoxCategorias;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblVencimiento;
+        private System.Windows.Forms.Label lblCategorias;
+    }
+}
