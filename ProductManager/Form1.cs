a45b9a694efa00660a94d01b75b75c2b80defe5f
JenniferG-Parrales <85895809+JenniferG-Parrales@users.noreply.github.com>
2021-12-09 10:26:48 -0600

    Add files via upload

---
+++ ProductManager/Form1.cs
@@ -0,0 +1,29 @@
+﻿using AppCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductManager
+{
+	public partial class Form1 : Form
+	{
+		private IProductServices Services{get;set;}
+	
+		public Form1(IProductServices services)
+		{
+			this.Services = services;
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
